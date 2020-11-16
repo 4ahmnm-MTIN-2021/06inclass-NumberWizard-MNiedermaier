328a505c0caf8eec1263dc6c08010dc21dbef462
mopvp <56366198+mopvp@users.noreply.github.com>
2020-11-16 11:11:09 +0100

    Add files via upload

---
+++ Assets/MyGame/Scripts/GameScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameScript : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+    public Text textGuess;
+    public Button btnHigher;
+    public Button btnLower;
+    public Button btnCorrect;
+
+    void Start()
+    {
+        max = 100;
+        min = 1;
+        guess = 50;
+
+        btnHigher.onClick.AddListener(Higher);
+        btnLower.onClick.AddListener(Lower);
+        btnCorrect.onClick.AddListener(Correct);
+
+    void Higher()
+        {
+            min = guess;
+            guessNext();
+            Debug.Log("Higher");
+        }
+    void Lower()
+        {
+            max = guess;
+            guessNext();
+            Debug.Log("Lower");
+        }
+    void Correct()
+        {
+            Debug.Log("correct Guess");
+            SceneManager.LoadScene("FinishedScene", LoadSceneMode.Single);
+        }
+    }
+
+
+    void guessNext()
+    {
+        guess = (min + max) / 2;
+        textGuess.text = guess.ToString();
+    }
+}
---
+++ README.md
@@ -0,0 +1,28 @@
+# 03inclass-NumberWizard-schiderf
+
+### Project description: 
+The goal is to create a Number guessing game called Number Wizard, where the computer guesses your chosen number through simple maths
+
+### Development platform: 
+Wiendows 10, Unity 202015f1, Visual Studio Code 2017
+
+### Target platform: 
+WebGL
+
+### Visuals: 
+Screenshots (concept and experience), Video
+
+### Necessary setup/execution steps: 
+Installation process, e.g. step by step instructions that I can run the project after cloning it
+
+### Third party material: 
+Unity Template for Unity202015f1 (2D)
+
+### Project state: 
+100%
+
+### Limitations: 
+
+### Lessons Learned: 
+
+Copyright by Moritz Niedermaier
